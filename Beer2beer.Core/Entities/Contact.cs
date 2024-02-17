74611ebccd8b4582e58587c0e66ef7b9790638a6
Ghislain Z <gzeleu@outlook.de>
2024-02-17 23:07:42 +0100

    Used this. setuup in editor config

---
+++ Beer2beer.Core/Entities/Contact.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Beer2beer.Core.Entities;
+
+public class Contact : Base<int>
+{
+    public string Email { get; set; } = string.Empty;
+}
---
+++ Beer2beer.Core/Entities/Supplier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beer2beer.Core.Entities;
+
+public class Supplier : Base<int>
+{
+    public Contact Email { get; set; } = string.Empty;
+}
